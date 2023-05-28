a1a706b1dcc305b0037966afd6ad923fbca6784f
dlaraaslan <dilara.aslan.98@hotmail.com>
2023-05-28 15:31:05 +0300

    add show controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :products
+  resources :shops, only:[:index,:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'products#index'
+  root 'shops#index'
 end
